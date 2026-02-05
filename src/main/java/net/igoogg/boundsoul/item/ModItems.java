5fa6921ae8cce37a19aa8bdb3852e94a4592ada1
Ignacy <157390108+IgooGG@users.noreply.github.com>
2026-02-05 13:57:35 +0100

    Update ModItems.java

--- src/main/java/net/igoogg/boundsoul/item/ModItems.java
+++ src/main/java/net/igoogg/boundsoul/item/ModItems.java
@@ -1,20 +1,25 @@
 package net.igoogg.boundsoul.item;
 
 import net.igoogg.boundsoul.BoundSoulMod;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.Identifier;
-import net.minecraft.world.item.Item;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item COLLAR = register("collar", new CollarItem(new Item.Properties().stacksTo(1)));
+    public static final Item COLLAR =
+            register("collar", new CollarItem(new Item.Settings().maxCount(1)));
 
     private static Item register(String name, Item item) {
-        return Registry.register(Registries.ITEM, new Identifier(BoundSoulMod.MOD_ID, name), item);
+        return Registry.register(
+                Registries.ITEM,
+                new Identifier(BoundSoulMod.MOD_ID, name),
+                item
+        );
     }
 
     public static void register() {
-        // Ensures class is loaded
+        // triggers class loading
     }
 }
