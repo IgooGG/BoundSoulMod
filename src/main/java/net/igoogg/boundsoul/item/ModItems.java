f07cd6c4a6db13c7bc2cba9a23d9f040fc083051
Ignacy <157390108+IgooGG@users.noreply.github.com>
2026-02-05 14:40:52 +0100

    Update ModItems.java

--- src/main/java/net/igoogg/boundsoul/item/ModItems.java
+++ src/main/java/net/igoogg/boundsoul/item/ModItems.java
@@ -1,5 +1,6 @@
 package net.igoogg.boundsoul.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.igoogg.boundsoul.BoundSoulMod;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
@@ -11,9 +12,11 @@ public class ModItems {
     public static final Item COLLAR =
             register("collar", new CollarItem(new Item.Settings().maxCount(1)));
 
-    public static void register() {
-        // static init is enough
-    }
+    public static final Item COLLAR_KEY =
+            register("collar_key", new CollarKeyItem(new Item.Settings().maxCount(1)));
+
+    public static final Item SHOCKER =
+            register("shocker", new ShockerItem(new Item.Settings().maxCount(1)));
 
     private static Item register(String name, Item item) {
         return Registry.register(
@@ -21,5 +24,9 @@ public class ModItems {
                 Identifier.of(BoundSoulMod.MOD_ID, name),
                 item
         );
+    }
+
+    public static void register() {
+        BoundSoulMod.LOGGER.info("Registering items for " + BoundSoulMod.MOD_ID);
     }
 }
